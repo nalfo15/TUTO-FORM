6104975cad2fa58a5b84193b5888feb90f678f15
Nathan <nathan.peytoud@proton.me>
2024-10-17 09:56:51 +0200

    Ajout code
    Ajout du code hello world

---
+++ Code/Hello_World/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace Hello_World
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCliquer = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnCliquer
+            // 
+            this.btnCliquer.AccessibleName = "btnCliquer";
+            this.btnCliquer.Location = new System.Drawing.Point(311, 179);
+            this.btnCliquer.Name = "btnCliquer";
+            this.btnCliquer.Size = new System.Drawing.Size(139, 62);
+            this.btnCliquer.TabIndex = 0;
+            this.btnCliquer.Text = "Cliquer ici";
+            this.btnCliquer.UseVisualStyleBackColor = true;
+            this.btnCliquer.Click += new System.EventHandler(this.btnCliquer_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(353, 264);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 16);
+            this.label1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnCliquer);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCliquer;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
